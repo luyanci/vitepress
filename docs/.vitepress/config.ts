25ec60e50cc3ae55aafa87d68eed5a734eb54ec8
YC酱luyancib <68143180+luyanci@users.noreply.github.com>
2024-05-13 22:51:24 +0800

    更新 config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
 	description: '基于 vitepress 的文档模板',
         head: [
         ['link', { rel: 'icon', href: '/favicon.png' }],
-        ['link', { rel: 'stylesheet', href: 'https://unpkg.com/remixicon@latest/fonts/remixicon.css'}]
+        ['link', { rel: 'stylesheet', href: 'https://unpkg.com/remixicon@latest/fonts/remixicon.css'}],
+        ['link', { rel: 'stylesheet', href: 'https://unpkg.com/lxgw-wenkai-screen-web@1.330.1/lxgwwenkaiscreen/result.min.css'}],
+
         ],
 	themeConfig: {
 		logo: 'logo.svg',
