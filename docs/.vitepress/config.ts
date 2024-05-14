29e9c5f289dab555b66f3dca35ea6f8dfc1ced57
YC酱luyancib <68143180+luyanci@users.noreply.github.com>
2024-05-14 18:33:56 +0800

    更新 config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,38 +1,40 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-	lang: 'zh-CN',
+        lang: 'zh-CN',
         base: '/vitepress/',
-	title: 'Hero',
-	description: '基于 vitepress 的文档模板',
+        title: 'Hero',
+        description: '基于 vitepress 的文档模板',
         head: [
         ['link', { rel: 'icon', href: '/favicon.png' }],
         ['link', { rel: 'stylesheet', href: 'https://unpkg.com/remixicon@latest/fonts/remixicon.css'}],
         ['link', { rel: 'stylesheet', href: 'https://unpkg.com/lxgw-wenkai-screen-web@1.330.1/lxgwwenkaiscreen/result.css'}],
-
         ],
-	themeConfig: {
-		logo: 'logo.svg',
-		footer: {
-			message: 'Released under the MIT License.',
-			copyright: 'Copyright © 2022 markthree'
-		},
-		nav: [
-			{
-				text: 'Guide',
-				link: '/guide/'
-			},
-			{
-				text: 'About',
-				link: '/about/'
-			}
-		],
-		socialLinks: [
-			{
-				icon: 'github',
-				link: 'https://github.com/name/repo'
-			}
-		],
+        themeConfig: {
+                logo: 'logo.svg',
+                fontFamily: {
+                        base: 'LXGW WenKai Screen'
+                },
+                footer: {
+                        message: 'Released under the MIT License.',
+                        copyright: 'Copyright © 2022 markthree'
+                },
+                nav: [
+                        {
+                                text: 'Guide',
+                                link: '/guide/'
+                        },
+                        {
+                                text: 'About',
+                                link: '/about/'
+                        }
+                ],
+                socialLinks: [
+                        {
+                                icon: 'github',
+                                link: 'https://github.com/name/repo'
+                        }
+                ],
 sidebar: [
       {
         //分组标题
@@ -43,5 +45,5 @@ sidebar: [
         ],
       },
     ],
-	}
+        }
 })
